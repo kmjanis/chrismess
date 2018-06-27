bda4d50d6aab7c9116bbe143269b1bb71c1471a5
Kelly Janis <kellymj98@gmail.com>
2018-06-27 16:30:16 -0400

    Davey's updates on js, css, html 6/27

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-const form = document.querySelector('form#flickForm')
-
-const renderProperty = function(name, value) {
-  const span = document.createElement('span')
-  span.classList.add(name)
-  span.textContent = value
-  return span
-}
-
-const handleSubmit = function(ev) {
-  ev.preventDefault()
-  const f = ev.target
-
-  const flickSpan = renderProperty('name', f.flickName.value)
-  const chrisSpan = renderProperty('chris', f.chrisName.value)
-
-  const item = document.createElement('li')
-  item.classList.add('flick')
-  item.appendChild(flickSpan)
-  item.appendChild(chrisSpan)
-
-  const list = document.querySelector('#flicks')
-  list.appendChild(item)
-
-  f.reset()
-}
-
-form.addEventListener('submit', handleSubmit)
+class App {
+    constructor() {
+      const form = document.querySelector('form#flickForm')
+      form.addEventListener('submit', (ev) => {
+        ev.preventDefault()
+        this.handleSubmit(ev)
+      })
+    }
+  
+    renderProperty(name, value) {
+      const span = document.createElement('span')
+      span.classList.add(name)
+      span.textContent = value
+      return span
+    }
+  
+    renderItem(flick) {
+      const item = document.createElement('li')
+      item.classList.add('flick')
+  
+      // get the list of properties
+      const properties = Object.keys(flick)
+  
+      // loop over each property
+      properties.forEach((propertyName) => {
+        // build a span
+        const span = this.renderProperty(propertyName, flick[propertyName])
+        item.appendChild(span)
+      })
+  
+      return item
+    }
+  
+    handleSubmit(ev) {
+      const f = ev.target
+  
+      const flick = {
+        name: f.flickName.value,
+        chris: f.chrisName.value,
+      }
+  
+      const item = this.renderItem(flick)
+  
+      const list = document.querySelector('#flicks')
+      list.appendChild(item)
+  
+      f.reset()
+      f.flickName.focus()
+    }
+  }
+  
+  const app = new App()
